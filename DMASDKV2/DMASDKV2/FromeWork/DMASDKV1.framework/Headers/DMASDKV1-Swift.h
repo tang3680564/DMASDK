2866b68a0726cacead04eae697731bd64bdf50cd
tangjianqiang <494326253@qq.com>
2020-01-03 10:27:09 +0800

    fix

--- DMASDKV2/DMASDKV2/FromeWork/DMASDKV1.framework/Headers/DMASDKV1-Swift.h
+++ DMASDKV2/DMASDKV2/FromeWork/DMASDKV1.framework/Headers/DMASDKV1-Swift.h
@@ -268,6 +268,13 @@ SWIFT_CLASS("_TtC8DMASDKV113PaymentConfig")
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
+
+SWIFT_CLASS("_TtC8DMASDKV113TokenContract")
+@interface TokenContract : NSObject
+- (nonnull instancetype)init SWIFT_UNAVAILABLE;
++ (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
+@end
+
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
 #endif
